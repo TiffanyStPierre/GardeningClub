42b5c77297f1eddcfc4eaea24c0420d86cd198a9
TiffanyStPierre <stpierre.tiffany@gmail.com>
2022-09-05 16:12:46 -0600

    form submit button javascript completed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
 
 function navToggle() {
     btn.classList.toggle('open');
@@ -8,3 +11,11 @@ function navToggle() {
 }
 
 btn.addEventListener('click', navToggle);
+
+document.getElementById('form-btn').addEventListener('click', function(event){
+    event.preventDefault()
+    alert('Thank you. Your membership is being processed.');
+    firstName.value = '';
+    lastName.value = '';
+    email.value = '';
+});
